77b4f20da966a54411d97044566759e4db4fd2a3
AngeloDalicandro <angelo.dalicandro1@gmail.com>
2022-06-16 15:51:41 +0200

    pseudocode

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 // Visualizzare in un alert 5 numeri casuali.
 // 30 secondi dopo la chiusura dell'alert, l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+// Salvo 5 numeri random in una array
+
+// Creo un array per salvare i numeri comunicati dall'utente
+
+// Creo un alert per comunicare all'utente in 5 numeri da memorizzare
+
+// Imposto il timer per far partire allo scadere dei 30 secondi un ciclo for
+
+//NEL CICLO FOR CHE SI RIPETE 5 VOLTE
+// Se il numeri digitati dall'utente è nell'array
+// Pusho la selezione dell'utente nel secondo array
+
+// Alla fine del ciclo comunico all'utente quali numeri ha ricordato 
+
+// Creo una funzione per generare dei numeri random
